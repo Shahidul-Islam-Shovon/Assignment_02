7ff81fc9944be3a2e1653a5f3739099809eab3fe
Shahidul-Islam-Shovon <shahidul.shovon.work@gmail.com>
2024-05-17 20:07:05 +0600

    string.php done

--- string.php
+++ string.php
@@ -1,10 +1,21 @@
-<?php 
+<?php
 
+function countVowels($str)
+{
+     $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+     $count = 0;
+     for ($i = 0; $i < strlen($str); $i++) {
+          if (in_array($str[$i], $vowels)) {
+               $count++;
+          }
+     }
+     return $count;
+}
 
+$strings = ["Hello", "World", "PHP", "Programming"];
 
-
-
-
-
-
-?>
+foreach ($strings as $string) {
+     $vowelCount = countVowels($string);
+     $reversedString = strrev($string);
+     echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reversedString <br>";
+}
